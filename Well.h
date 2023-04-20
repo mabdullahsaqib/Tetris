28f0269fb85530e2644f2b1b15b5e3a723855b8f
AIbdullah <130112308+M-A-S-AI@users.noreply.github.com>
2023-04-20 22:21:50 +0500

    Fixed the grid and movement.

--- Well.h
+++ Well.h
@@ -39,17 +39,12 @@ inline void Well::Board(RenderWindow& window)
 	bg.setFillColor(Color(30, 30, 30));
 
 	RectangleShape Grid;
-	Grid.setSize(Vector2f(515.0f, 800.0f));
+	Grid.setSize(Vector2f(530.0f, 805.0f));
 	Grid.setPosition(0.0f, 5.0f);
 	Grid.setFillColor(Color(255, 255, 255));
 	Texture grid;
 	grid.loadFromFile("Textures/Grid 10x20.png");
 	Grid.setTexture(&grid);
-
-	//RectangleShape firstshape;
-	//firstshape.setSize(Vector2f(45.0f, 33.0f));
-	//firstshape.setPosition(3.0f, 8.0f);
-	//firstshape.setFillColor(Color::Cyan);
 
 	Texture blue;
 	blue.loadFromFile("Textures/Tetromino_I.png");
@@ -82,17 +77,15 @@ inline void Well::Board(RenderWindow& window)
 			if (x < 466)
 			{
 				x += y;
-				y -= 0.3f;
 				rectangleshape.setPosition(x, v);
 			}
 			switchtime = 0;
 		}
 		if (Keyboard::isKeyPressed(Keyboard::Key::Left) && switchtime > 0.2)
 		{
-			if (x >56)
+			if (x >50)
 			{
 				x -= y;
-				y += 0.3f;
 				rectangleshape.setPosition(x, v);
 			}
 			switchtime = 0;

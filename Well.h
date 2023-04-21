d307e04f86cd67080515da04cc8ffcd6e9865ebb
AIbdullah <130112308+M-A-S-AI@users.noreply.github.com>
2023-04-21 15:37:53 +0500

    made float

--- Well.h
+++ Well.h
@@ -111,7 +111,7 @@ inline void Well::Board(RenderWindow& window)
 					blue_tetromino[i].setScale(Vector2f(0.55f, 0.5f));
 					blue_tetromino[i].setTexture(blue);
 					blue_tetromino[i].setPosition(x, z);
-					z += 40.5;
+					z += 40.5f;
 				}
 				z = y;
 			}
